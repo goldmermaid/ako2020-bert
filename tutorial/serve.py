9832e1dbc121af3603c198b7156b2c01dcbacb78
Rachel Hu <goldpiggy@berkeley.edu>
2020-01-13 21:06:23 +0000

    question

--- tutorial/serve.py
+++ tutorial/serve.py
@@ -1,12 +1,15 @@
 import collections, json, logging, warnings
+import multiprocessing as mp
+from functools import partial
+
 import gluonnlp as nlp
 import mxnet as mx
 from mxnet.gluon import Block, nn
-from bert.data.qa import preprocess_dataset
-from bert.bert_qa_evaluate import PredResult, predict
+import bert
+from bert.data.qa import preprocess_dataset, SQuADTransform
+import bert_qa_evaluate
 
-import multiprocessing as mp
-from functools import partial
+
 
 class BertForQA(Block):
     """Model for SQuAD task with BERT.
@@ -48,26 +51,45 @@ class BertForQA(Block):
         output = self.span_classifier(bert_output)
         return output
     
-def get_all_results(net, dev_data_transform):
+    
+def get_all_results(net, vocab, squadTransform, test_dataset, ctx = mx.cpu()):
     all_results = collections.defaultdict(list)
+    
+    def _vocab_lookup(example_id, subwords, type_ids, length, start, end):
+        indices = vocab[subwords]
+        return example_id, indices, type_ids, length, start, end
+    
+    dev_data_transform, _ = preprocess_dataset(test_dataset, squadTransform)
+    dev_data_transform = dev_data_transform.transform(_vocab_lookup, lazy=False)
     dev_dataloader = mx.gluon.data.DataLoader(dev_data_transform, batch_size=1, shuffle=False)
+    
     for data in dev_dataloader:
         example_ids, inputs, token_types, valid_length, _, _ = data
         batch_size = inputs.shape[0]
         output = net(inputs.astype('float32').as_in_context(ctx),
-                                   token_types.astype('float32').as_in_context(ctx),
-                                   valid_length.astype('float32').as_in_context(ctx))
+                     token_types.astype('float32').as_in_context(ctx),
+                     valid_length.astype('float32').as_in_context(ctx))
         pred_start, pred_end = mx.nd.split(output, axis=2, num_outputs=2)
         example_ids = example_ids.asnumpy().tolist()
         pred_start = pred_start.reshape(batch_size, -1).asnumpy()
         pred_end = pred_end.reshape(batch_size, -1).asnumpy()
 
         for example_id, start, end in zip(example_ids, pred_start, pred_end):
-            all_results[example_id].append(PredResult(start=start, end=end))
+            all_results[example_id].append(bert_qa_evaluate.PredResult(start=start, end=end))
     return(all_results)
 
 
-def model_fn(model_dir):
+def _test_example_transform(test_examples):
+    test_examples_tuples = []
+    i = 0
+    for test in test_examples:
+        tup = (i, "", test[0], test[1], [], [])
+        test_examples_tuples.append(tup)
+        i += 1
+    return(test_examples_tuples)
+
+
+def model_fn(model_dir = "", params_path = "bert_qa-7eb11865.params"):
     """
     Load the gluon model. Called once when hosting service starts.
     :param: model_dir The directory where model files are stored.
@@ -80,14 +102,16 @@ def model_fn(model_dir):
                                         use_pooler=False,
                                         pretrained=False)
     net = BertForQA(bert_model)
-    net.load_parameters("temp/bert_qa-7eb11865.params", ctx=mx.cpu())
+    if len(model_dir) > 0:
+        params_path = model_dir + "/" +params_path
+    net.load_parameters(params_path, ctx=mx.cpu())
     
-    tokenizer = nlp.data.BERTTokenizer(vocab)
-    transform = bert.data.qa.SQuADTransform(tokenizer, is_pad=False, is_training=False, do_lookup=False)
+    tokenizer = nlp.data.BERTTokenizer(vocab,  lower=True)
+    transform = SQuADTransform(tokenizer, is_pad=False, is_training=False, do_lookup=False)
     return net, vocab, transform
 
 
-def transform_fn(model, data, input_content_type, output_content_type):
+def transform_fn(model, data_file, input_content_type=None, output_content_type=None):
     """
     Transform a request using the Gluon model. Called once per request.
     :param model: The Gluon model and the vocab
@@ -116,33 +140,31 @@ def transform_fn(model, data, input_content_type, output_content_type):
         in the San Francisco Bay Area at Santa Clara, California."], 
         
         [403, 355, 355])
-        
 
-        
     :param input_content_type: The request content type, assume json
     :param output_content_type: The (desired) response content type, assume json
     :return: response payload and content type.
     """
-    
-    # we can use content types to vary input/output handling, but
-    # here we just assume json for both
-    net, vocabulary, transform = model
-    dataset = json.loads(data)
-#     dev_data_transform, _ = preprocess_dataset(dataset, transform)
-#     dev_data_transform = dev_data_transform.transform(vocab_lookup, lazy=False)
-    dev_data_transform = dataset.transform(transform._transform)
+    net, vocab, squadTransform = model
+    if data_file[:-4] == ".json":
+        with open(data_file) as data:    
+            test_examples = json.load(data)
+    else:
+        test_examples = json.loads(data_file)
+    test_examples_tuples = _test_example_transform(test_examples)
+    test_dataset = mx.gluon.data.SimpleDataset(test_examples_tuples)
+    all_results = get_all_results(net, vocab, squadTransform, test_dataset, ctx=mx.cpu())
+
     
     all_predictions = collections.defaultdict(list) # collections.OrderedDict()
-    all_results = get_all_results(net, dev_data_transform)
-    for features in dev_data_transform:
+    data_transform = test_dataset.transform(squadTransform._transform)
+    for features in data_transform:
         f_id = features[0].example_id
         results = all_results[f_id]
-    
-        prediction, nbest = predict(
+        prediction, nbest = bert_qa_evaluate.predict(
             features=features,
             results=results,
-            tokenizer=nlp.data.BERTBasicTokenizer(lower=True))
-        
+            tokenizer=nlp.data.BERTBasicTokenizer(vocab))        
         nbest_prediction = [] 
         for i in range(3):
             nbest_prediction.append('%.2f%% \t %s'%(nbest[i][1] * 100, nbest[i][0]))
